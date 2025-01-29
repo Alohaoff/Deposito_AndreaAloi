c405669117fcd7d8abd577b06f82249e28d3ee0f
Alohaoff <andreaaloi5@icloud.com>
2025-01-29 18:09:52 +0100

    Caricamento EsercizioComplte

--- EsercizioCompleto.py
+++ EsercizioCompleto.py
@@ -1,9 +1,9 @@
 while True:
-    scelta1 = input("Vuoi entrare nel menu: "). strip().lower()
-    if scelta1 == "si":
+    scelta_entrata = input("Vuoi entrare nel menu: "). strip().lower()
+    if scelta_entrata == "si":
         break
     else:
-        print("Uscita dal programma.")
+        print("Uscita dal programma")
 
 controllo = True       
 while controllo:
@@ -18,12 +18,48 @@ while True:
     print("1. Calcola la somma dei numeri pari da 1 al numero scelto")
     print("2. Stampa tutti i numeri dispari da 1 al numero scelto")
     print("3. Verifica se il numero scelto è primo")
-    print("4. Esci")
+    print("4. Cambia il numero scelto")
+    print("5. Esci")
     scelta = input("Scegli un'opzione: ")
     
     if scelta == "1":
-            sommapari = 0
-            for x in range(2, numero, 2):
+        sommapari = 0
+        for x in range(1, numero+1):
+            if x % 2 ==0:
                 sommapari += x
-            print("La somma dei numeri pari: ", sommapari)
+        print("La somma dei numeri pari: ", sommapari)
+    
+    elif scelta == "2":
+        listadispari =[]
+        for y in range(1, numero+1):
+            if y % 2 != 0:
+                listadispari.append(y)
+        print("I numeri dispari sono:", listadispari)
+            
+    elif scelta == "3":
+        if numero < 2:
+            print("Il numero scelto non è primo")
+        else:
+            primo = True
+            for z in range(2, numero):
+                if numero % z == 0:
+                    primo = False
+            if primo:
+                print("Il numero scelto è primo")
+            else:
+                print("Il numero scelto non è un numero primo")
         
+    elif scelta == "4":
+        controllo = True       
+        while controllo:
+            numero= int(input("Inserisci un numero intero positivo: "))
+            if numero <= 0:
+                print("Spiacente, devi inserire un numero positivo!")
+            else:
+                break
+    
+    elif scelta =="5":
+        print("Uscita dal programma")
+    
+    else:
+        print("Scelta non valida")
